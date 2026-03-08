824433edb343472ade6d501f3c8296750b3c2ecc
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-08 16:48:09 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
 		<>
 			<div className="min-h-screen flex items-center justify-center bg-gray-100">
 				<div className="bg-white rounded-xl shadow-lg p-6 w-80">
-					<img className="rounded-lg mb-4"></img>
+					<img className="rounded-lg mb-4" src="./dimonds.jpeg"/>
 					<h1 className="text-2x1 font-bold mb-2">Dimonds</h1>
 					<p className="text-green-600 mb4">$9000.99</p>
 					<button className="bg-blue-500 text-white px-4 py-2">Buy Now</button>
