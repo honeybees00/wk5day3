71248f1575ea088de36569601658c104dfb0719a
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-07 23:46:35 -0500

    updated formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-
-import './App.css'
+import "./App.css";
 
 export default function App() {
- 
-return (
-  <>
-  <div className="min-h-screen flex items-center justify-center bg-gray-100">
-  <div className="bg-white rounded-xl shadow-lg p-6 w-80">
-  
-  <img className='rounded-lg mb-4'></img>
-  <h1 className="text-2x1 font-bold mb-2">Dimonds</h1>
-  <p className="text-green-600 mb4">$9000.99</p>
-  <button className='bg-blue-500 text-white px-4 py-2'>Buy Now</button>
-
-    
-    
-    </div>
-    </div>  
-  
-
-</>
-)
+	return (
+		<>
+			<div className="min-h-screen flex items-center justify-center bg-gray-100">
+				<div className="bg-white rounded-xl shadow-lg p-6 w-80">
+					<img className="rounded-lg mb-4"></img>
+					<h1 className="text-2x1 font-bold mb-2">Dimonds</h1>
+					<p className="text-green-600 mb4">$9000.99</p>
+					<button className="bg-blue-500 text-white px-4 py-2">Buy Now</button>
+				</div>
+			</div>
+		</>
+	);
 }
